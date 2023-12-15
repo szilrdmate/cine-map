3b78275a06e80debeabde956ab2b8885231ba1ea
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-15 13:25:05 +0100

    Functional card comp

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,32 +1,35 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import movieLocations from "../utils/movieLocations.json";
 import cityCoordinates from "../utils/cities";
+import MovieCard from "./MovieCard"
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 const MapComponent = ({ city }) => {
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     if (!mapRef.current) {
+      // Initialize the map only once
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
         style: "mapbox://styles/mapbox/standard-beta",
-        zoom: 14,
+        zoom: 16,
       });
     }
 
     if (!cityCoordinates[city]) return;
     const { lat, lng } = cityCoordinates[city];
-
+  
     const map = mapRef.current;
     map.jumpTo({ center: [lng, lat], zoom: 16, pitch: 62, bearing: -20 });
     
   
-    map.on('style.load', () => {
+    const handleStyleLoad = () => {
       map.setConfigProperty("basemap", "lightPreset", "dusk")
       map.setConfigProperty('basemap', "showPlaceLabels", false)
       map.setConfigProperty('basemap', "showRoadLabels", false)
@@ -36,23 +39,35 @@ const MapComponent = ({ city }) => {
       // Add markers and popups for movie locations
       movieLocations.forEach((movie) => {
         movie.locations.forEach((location) => {
+          // Create a HTML element for each feature (marker)
           const markerEl = document.createElement('div');
+          markerEl.className = 'cursor-pointer';
           markerEl.textContent = '📍';
           markerEl.style.fontSize = '2.5em';
-  
-          const marker = new mapboxgl.Marker(markerEl)
+      
+          // Create a marker at the given location
+          new mapboxgl.Marker(markerEl)
             .setLngLat([location.lng, location.lat])
-            .setPopup(
-              new mapboxgl.Popup({ offset: 0 })
-                .setHTML(`<h3 style="color: black;">${location.name}</h3>`)
-            )
             .addTo(map);
+      
+          // Add a click event listener to the marker
+          markerEl.addEventListener('click', () => {
+            setSelectedMovie({
+              title: movie.title,
+              name: location.name,
+              lat: location.lat,
+              lng: location.lng
+            });
+          });
         });
       });
-    });
+    }
+
+    map.on('style.load', handleStyleLoad);
 
     return () => {
       if (mapRef.current) {
+        mapRef.current.off('style.load', handleStyleLoad);
         mapRef.current.remove();
         mapRef.current = null;
       }
@@ -69,9 +84,14 @@ const MapComponent = ({ city }) => {
     }
   };
 
+  const closeCard = () => {
+    setSelectedMovie(null);
+  };
+
   return (
     <div>
-      <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />
+      <div ref={mapContainerRef} style={{ width: "100vw", height: "100vh"}} />
+      {selectedMovie && <MovieCard movie={selectedMovie} onClose={closeCard} />}
       <button onClick={toggleView} className="absolute bottom-44 right-12 bg-white rounded-full px-4 py-2 shadow-xl border-[1px] border-solid border-gray-400 font-bold">
         2D/3D
       </button>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,29 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = ({ title, year, location, image, isOpen, onClose }) => {
-  const cardStyle = {
-    transform: isOpen ? 'translateX(0)' : 'translateX(-100%)', // Slide in from the left when isOpen is true
-    transition: 'transform 0.3s ease-in-out', // Smooth transition
-  };
+const MovieCard = ({ movie, onClose }) => {
+  if (!movie) return null;
+
 
   return (
-    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 w-64 bg-white h-full shadow-lg" style={cardStyle}>
-      <div className="max-w-sm rounded overflow-hidden">
-        <img className="w-full" src={image} alt={`Scene from ${title}`} />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <h2 className="text-gray-700">{title}</h2>
-          <p className="text-gray-700 text-base">Year: {year}</p>
-          <p className="text-gray-700 text-base">Location: {location}</p>
-        </div>
-        <div className="px-6 py-4">
-          <button onClick={onClose} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Close
-          </button>
-        </div>
-      </div>
+    <div className="movie-card absolute top-3 left-3 p-8 text-white bg-teal-950 shadow-xl z-20 rounded-xl">
+      <button onClick={onClose} className="close-btn absolute top-2 right-2 bg-none border-none text-xl cursor-pointer">X</button>
+      <img src="" />
+      <h2 className="font-bold text-2xl">{movie.title}</h2>
+      <p>Location: {movie.name}</p>
+      <p>Latitude: {movie.lat}°</p>
+      <p>Longitude: {movie.lng}°</p>
     </div>
   );
 };
+
 
 export default MovieCard;
