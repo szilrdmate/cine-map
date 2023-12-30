e5efbe73bafba7de4159d488d49d2edaed98a094
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-30 11:01:40 +0700

    Erorr handling

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,3 +1,5 @@
+// src/components/MapComponent.jsx
+
 import { useEffect, useRef, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import mapboxgl from "mapbox-gl";
@@ -7,6 +9,7 @@ import MovieCard from "./MovieCard";
 import { setSelectedMovie } from "../utils/store.js";
 import { useMediaQuery } from "react-responsive";
 
+// Mapbox token stored in as a .env variable
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 const MapComponent = () => {
@@ -46,7 +49,7 @@ const MapComponent = () => {
     map.current.setConfigProperty("basemap", "showTransitLabels", false);
 
     // Add markers and popups for movie locations
-    movieLocations.features.forEach((feature) => {
+    {/*movieLocations.features.forEach((feature) => {
       // Add markers and popups for movie locations
       const { geometry } = feature;
       const markerEl = document.createElement("div");
@@ -62,15 +65,15 @@ const MapComponent = () => {
         console.log("Marker clicked, dispatching movie:", feature);
         dispatch(setSelectedMovie(feature));
       });
-    });
-  }, [dispatch]);
+    });*/}
+  }, []);
 
   // useEffect to initialize the map
   useEffect(() => {
     console.log("useEffect running");
-    
+
     // Initialize the map only if it's not already created
-    if (map.current) return
+    if (map.current) return;
 
     try {
       map.current = new mapboxgl.Map({
@@ -81,29 +84,111 @@ const MapComponent = () => {
         center: [2.29392, 48.85934],
       });
 
-      map.current.on("style.load", handleStyleLoad);
+      // map.current.on("style.load", handleStyleLoad);
 
-      map.current.on('error', (e) => {
-        // Handle map errors
-        console.error('Map error:', e);
-        // Optionally, update state to show an error message to the user
+      map.current.on("style.load", () => {
+
+        handleStyleLoad();
+
+        map.current.loadImage("marker_red.png", (error, image) => {
+          // TODO: erro handling: if (error) throw error;
+
+          // Add the image to the map style
+          map.current.addImage("custom-marker", image);
+
+          map.current.addSource("movies", {
+            type: "geojson",
+            data: movieLocations,
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom level to cluster points
+            clusterRadius: 50, // Radius of each cluster when clustering points
+          });
+
+          // Add a layer for the clusters themselves
+          map.current.addLayer({
+            id: "clusters",
+            type: "symbol",
+            source: "movies",
+            filter: ["has", "point_count"], // Filter for cluster points
+            layout: {
+              "icon-image": "custom-marker", // Use the same custom marker icon for clusters
+              "icon-size": 1.5, 
+            }
+          });
+
+          // Add a layer for the unclustered markers
+          map.current.addLayer({
+            id: "unclustered-markers",
+            type: "symbol",
+            source: "movies",
+            filter: ["!", ["has", "point_count"]], // Filter for non-cluster points
+            layout: {
+              "icon-image": "custom-marker", // Use a custom marker icon
+              "icon-size": 1.5,
+            },
+          });
+
+          // Event handler for clicking on a cluster
+          map.current.on("click", "clusters", (e) => {
+            const features = map.current.queryRenderedFeatures(e.point, {
+              layers: ["clusters"],
+            });
+            const clusterId = features[0].properties.cluster_id;
+            map.current
+              .getSource("movies")
+              .getClusterExpansionZoom(clusterId, (err, zoom) => {
+                if (err) return;
+                map.current.easeTo({
+                  center: features[0].geometry.coordinates,
+                  zoom: zoom,
+                });
+              });
+          });
+
+          // Event handler for clicking on individual markers
+          map.current.on("click", "unclustered-markers", (e) => {
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const feature = e.features[0];
+
+            console.log("Marker clicked, dispatching movie:", feature);
+            dispatch(setSelectedMovie(feature));
+          });
+
+          // Add the 'pointer' cursor style on hover over the clusters
+          map.current.on("mouseenter", "clusters", () => {
+            map.current.getCanvas().style.cursor = "pointer";
+          });
+          map.current.on("mouseenter", "unclustered-markers", () => {
+            map.current.getCanvas().style.cursor = "pointer";
+          });
+          map.current.on("mouseleave", "clusters", () => {
+            map.current.getCanvas().style.cursor = "";
+          });
+          map.current.on("mouseleave", "unclustered-markers", () => {
+            map.current.getCanvas().style.cursor = "";
+          });
+        });
+
+        map.current.on("error", (e) => {
+          // TODO: Handle map errors
+          console.error("Map error:", e);
+          // TODO: Optionally, update state to show an error message to the user
+        });
       });
-    
-        // ... rest of the initialization code
     } catch (error) {
-      console.error('Error initializing map:', error);
-      // Handle initialization errors
-      // Update state to inform user, if necessary
+      console.error("Error initializing map:", error);
+      // TODO: Handle initialization errors
+      // TODO: Update state to inform user, if necessary
     }
 
-    // Cleanup function to remove the map  
+    // Cleanup function to remove the map
     return () => {
       if (map.current) {
         map.current.remove();
         map.current = null;
       }
     };
-  }, [handleStyleLoad]);
+  }, [handleStyleLoad, dispatch]);
 
   // Handle city location changes
   useEffect(() => {
@@ -115,7 +200,10 @@ const MapComponent = () => {
         });
         break;
       default:
-        map.current.flyTo({ center: [2.29392, 48.85934], zoom: 16 });
+        map.current.flyTo({
+          center: [2.29392, 48.85934],
+          zoom: 16,
+        });
     }
   }, [type, coordinates, city]);
 
