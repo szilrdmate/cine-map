c3be2ba4f386973071338e41035ff38b6d22813e
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-31 18:16:33 +0700

    Debug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,14 @@
 // src/pages/Home.jsx
-
-import { useParams } from 'react-router-dom';
 import MapComponent from '../components/MapComponent';
 import Footer from '../components/Footer';
 import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
-  const { city } = useParams();
   const isDesktop = useMediaQuery( {query: '(min-width: 768px)'} );
-
-  const defaultCity = 'paris';
-  const currentCity = city || defaultCity;
-
+  
   return (
     <div>
-      <MapComponent city={currentCity} />
+      <MapComponent />
       {isDesktop && (<Footer />)}
     </div>
   );
